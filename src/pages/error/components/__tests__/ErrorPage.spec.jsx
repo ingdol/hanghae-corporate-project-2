41b596acaa4a4c2a22429cb5b3291f350aa09801
ingdol <dmswl3073@naver.com>
2024-10-14 23:08:00 +0900

    feat: 컴포넌트 단위 테스트 코드 추가 (#2)

--- src/pages/error/components/__tests__/ErrorPage.spec.jsx
+++ src/pages/error/components/__tests__/ErrorPage.spec.jsx
@@ -3,14 +3,27 @@ import { vi } from 'vitest';
 
 import render from '@/utils/test/render';
 import { ErrorPage } from '../ErrorPage';
+import { useNavigate } from 'react-router-dom';
 
 // 실제 모듈을 모킹한 모듈로 대체하여 테스트 실행 (react-router-dom의 useNavigate 모킹)
-
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useNavigate: vi.fn(),
+  };
+});
 it('"뒤로 이동" 버튼 클릭시 뒤로 이동하는 navigate(-1) 함수가 호출된다', async () => {
+  const navigate = vi.fn();
+  useNavigate.mockReturnValue(navigate);
+
   // Arrange: ErrorPage 컴포넌트를 렌더링
   const { user } = await render(<ErrorPage />);
 
   // Act: "뒤로 이동" 버튼을 클릭
+  const backButton = screen.getByRole('button', { name: /뒤로 이동/i });
+  await user.click(backButton);
 
   // Assert: navigate 함수가 -1 인자로 호출되었는지 확인
+  expect(navigate).toHaveBeenCalledWith(-1);
 });
